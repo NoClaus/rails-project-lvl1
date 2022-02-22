6aa5b094b7450b2562699c8ff83066d43c75023d
NoClaus <support@marketer.consulting>
2022-02-22 09:04:31 +0000

    Step 5: Some style fixing

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require_relative "hexlet_code/version"
+require_relative 'hexlet_code/version'
 
 # This module generates html forms
 module HexletCode
@@ -13,11 +13,11 @@ module HexletCode
       hash.each do |key, value|
         line += " #{key}=\"#{value}\"" if key != :selected
       end
-      line += " selected" if hash[:selected]
+      line += ' selected' if hash[:selected]
       line += if block_given?
                 ">#{yield}</#{tag}>"
               else
-                ">"
+                '>'
               end
     end
   end
@@ -28,16 +28,16 @@ module HexletCode
 
     def initialize(user)
       @user = user
-      @fields = ""
+      @fields = ''
     end
 
     def select_constructor(field_name, collection)
-      Tag.build("select", name: field_name) do
+      Tag.build('select', name: field_name) do
         options = collection.each_with_object([]) do |element, object|
           object << if user.gender == element
-                      Tag.build("option", value: element, selected: true) { element }
+                      Tag.build('option', value: element, selected: true) { element }
                     else
-                      Tag.build("option", value: element) { element }
+                      Tag.build('option', value: element) { element }
                     end
         end
         options.join
@@ -47,18 +47,18 @@ module HexletCode
     def input(field_name, as: nil, collection: [])
       @fields += case as
                  when :text
-                   Tag.build("textarea", cols: 20, rows: 40, name: field_name) { user.send(field_name) }
+                   Tag.build('textarea', cols: 20, rows: 40, name: field_name) { user.send(field_name) }
                  when :select
                    select_constructor(field_name, collection)
                  else
-                   Tag.build("input", type: "text", value: user.send(field_name), name: field_name)
+                   Tag.build('input', type: 'text', value: user.send(field_name), name: field_name)
                  end
     end
   end
 
-  def self.form_for(user, url: "#")
+  def self.form_for(user, url: '#')
     form = Form.new(user)
-    Tag.build("form", action: url, method: "post") do
+    Tag.build('form', action: url, method: 'post') do
       yield form
     end
   end
